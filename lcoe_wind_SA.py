d1c15067da5dc841b57e5825614ad7a12e2d569a
kilojoules <jaq8@humboldt.edu>
2015-07-21 13:28:04 -0600

    updating sa

--- lcoe_wind_SA.py
+++ lcoe_wind_SA.py
@@ -1,6 +1,6 @@
 #from openmdao.main.datatypes.api import Float
-from openmdao.main.api import Component, Assembly
 from dakota_driver.driver import pydakdriver
+from openmdao.main.api import Component, Assembly
 from wisdem.lcoe.lcoe_csm_assembly import lcoe_csm_assembly
 
 class lcoe_wind_SA(Assembly):
@@ -11,10 +11,15 @@ class lcoe_wind_SA(Assembly):
         self.add('lcoe_csm', lcoe_csm_assembly())
 
         # ..using the cutting edge pydakdriver
-        river = self.add('driver',pydakdriver())
+        dakDriver = pydakdriver()
+        dakDriver.UQ(seed=3232)
+        driver = self.add('driver',dakDriver)
         driver.workflow.add('lcoe_csm')
         driver.stdout = 'dakota.out'
         driver.stderr = 'dakota.err'
+
+        driver.add_parameter('lcoe_csm.weibull_k',low=2.6*0.5, high=2.6*1.5)
+        driver.add_parameter('lcoe_csm.shear_exponent',low=.1*0.5, high=.1*1.5)
 
         # use same objectives as previous sensitivity analysis
         driver.add_objective('lcoe_csm.coe')
@@ -25,3 +30,12 @@ class lcoe_wind_SA(Assembly):
         driver.add_objective('lcoe_csm.turbine_cost')
         driver.add_objective('lcoe_csm.turbine_mass')
         driver.add_objective('lcoe_csm.rated_rotor_speed')
+
+
+if __name__ == '__main__':
+
+    # LCOE sensitivity analysis.
+
+    top = lcoe_wind_SA()
+    top.lcoe_csm.advanced_blade = True
+    top.run()
